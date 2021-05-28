e7fb6a99b6ba11df460542d7ca812b74c98f330a
Loi Van Tran <datloiboi@gmail.com>
2021-05-28 10:44:40 +0700

    Removed unneeded files

--- server/routes/auth.api.js
+++
@@ -1,8 +0,0 @@
-var express = require("express");
-var router = express.Router();
-
-const authController = require("../controllers/auth.controller");
-
-router.post("/login", authController.loginWithEmail);
-
-module.exports = router;

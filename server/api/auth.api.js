0c4a89e45f2e13335c83eb3da9ec9b158b33563b
Loi Van Tran <datloiboi@gmail.com>
2021-06-03 09:40:14 +0700

    Update api

--- server/api/auth.api.js
+++ server/api/auth.api.js
@@ -4,6 +4,5 @@ var router = express.Router();
 const authController = require("../controllers/auth.controller");
 
 router.post("/login", authController.loginWithEmail);
-router.get("/production-apo", authController.production);
 
 module.exports = router;
